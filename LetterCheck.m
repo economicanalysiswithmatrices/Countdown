7fd937e9e93ecc48e41c036c82844cc51aac9617
Chris Finch <33253740+finchchris@users.noreply.github.com>
2018-01-09 12:59:46 +0000

    Added some comments

--- LetterCheck.m
+++ LetterCheck.m
@@ -1,10 +1,10 @@
-function Boolean = LetterCheck(Answer)
+function Boolean = LetterCheck(Answer) % funtion to check if letters used correlate with letters chosen  
 
-Letters = '';
+Letters = ''; % defines 'Letters' variable 
 
-for i=1:9
-   LetterNumber = strcat('Letter',int2str(i));
-   LetterObject = findobj('Style','text','-and','Tag',LetterNumber);
+for i=1:9 % for letters 1 to 9
+   LetterNumber = strcat('Letter',int2str(i)); % sets 'LetterNumber' equal to i
+   LetterObject = findobj('Style','text','-and','Tag',LetterNumber); % finds 'LetterObject' with 'LetterNumber' = i 
    Letter = get(LetterObject,'String');
    Letters = strcat(Letters,Letter); 
 end
@@ -12,7 +12,7 @@ end
 
 Answer = upper(Answer);
 
-AnswerLength = length(Answer);
+AnswerLength = length(Answer); % sets 'AnswerLength' variable to equal length of Answer 
 
 Check = 0;
 
