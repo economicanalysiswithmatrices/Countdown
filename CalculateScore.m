90a2b62507286c5a279bcb0a10b8420d4fbda051
Chris Finch <33253740+finchchris@users.noreply.github.com>
2018-01-09 13:33:26 +0000

    Added comments and changed function name

---
+++ CalculateScore.m
@@ -0,0 +1,5 @@
+function Score = CalculateScore(Answer) % calculates score for word 
+
+Score = length(Answer); % set variable 'Score' equal to length of 'Answer'
+
+Score = num2str(Score); % converts 'Score' to numeric array
