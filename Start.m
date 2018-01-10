d313920cfa8381dc3ce4a276bd911fa9105d0179
Chris Finch <33253740+finchchris@users.noreply.github.com>
2018-01-10 10:36:20 +0000

    Added comments

--- Start.m
+++ Start.m
@@ -1,30 +1,27 @@
 function Start
 
-global Started;
+global Started; % obtains global variable 'Started' 
+Started = true; % sets 'Started' to eequal true 
 
-Started = true;
+TimerObject = findobj('Style','text','-and','Tag','Timer'); % obtains 'Timer' object from GUI
 
-TimerObject = findobj('Style','text','-and','Tag','Timer');
+[y,Fs] = audioread('Countdown.wav'); % extracts audio file 'Countdown.wav' from 
+sound(y,Fs); % plays audio file
 
-[y,Fs] = audioread('Countdown.wav');
-sound(y,Fs);
-
-for i=1:30
-    set(TimerObject, 'String', 30-i)
-    pause(1)
+for i=1:30 
+    set(TimerObject, 'String', 30-i) % reduces timer by 1 
+    pause(1) % with 1 second pause inbetween each reduction 
 end
 
 AnswerObject = findobj('Style','text','-and','Tag','Answer');
-
 Answer = get(AnswerObject,'String');
 
-
-if(LetterCheck(Answer) && (WordCheck(Answer)))
-    Score = CalculateScore(Answer);
-    SaveScore(Score,Answer)
-    Reset
+if(LetterCheck(Answer) && (WordCheck(Answer))) % checks Letter and Word criteria is met 
+    Score = CalculateScore(Answer); % calculate 'Score'
+    SaveScore(Score,Answer) % executes 'SaveScore' function
+    Reset % executes 'Reset' function 
 else
-    msgbox('Your word was invalid');
-    Reset
+    msgbox('Your word was invalid'); % displays message 'Your word was invalid'
+    Reset % executes 'Reset' function 
     
 end
