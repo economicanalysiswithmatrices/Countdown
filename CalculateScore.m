a7011142c5998036a28ef4cbc4c2809b3d1f9128
Chris Finch <33253740+finchchris@users.noreply.github.com>
2018-01-10 11:20:21 +0000

    Update CalculateScore.m

--- CalculateScore.m
+++ CalculateScore.m
@@ -2,4 +2,4 @@ function Score = CalculateScore(Answer) % calculates score for word
 
 Score = length(Answer); % set variable 'Score' equal to length of 'Answer'
 
-Score = num2str(Score); % converts 'Score' to numeric array
+Score = num2str(Score); % converts 'Score' to an integer
