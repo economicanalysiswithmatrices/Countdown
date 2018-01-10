8b9ccfedb25ca21c0f571b0d9e5ff9c5786c3282
Chris Finch <33253740+finchchris@users.noreply.github.com>
2018-01-10 18:20:42 +0000

    Updated Start.m
    No errors if program is closed when timer is active, audio stops when program is closed.

--- Start.m
+++ Start.m
@@ -1,12 +1,18 @@
 function Start
 
 global Started;  
+
+try %We need to use a try and catch otherwise if the timer is running when the program is closed it will cause many errors
+    
 Started = true; % sets 'Started' to equal true 
 
 TimerObject = findobj('Style','text','-and','Tag','Timer'); % obtains 'Timer' from GUI
 
 [y,Fs] = audioread('Countdown.wav'); % extracts audio file 'Countdown.wav' 
 sound(y,Fs); % plays 'Countdown.wav' audio file
+
+    
+
 
 for i=1:30 
     set(TimerObject, 'String', 30-i) % set timer to countdown
@@ -24,4 +30,8 @@ else
     msgbox('Your word was invalid'); % displays message 'Your word was invalid'
     Reset % runs 'Reset' function 
     
+end
+
+catch %This is called if the timer is started and the program is closed, it stops the audio
+    clear sound;
 end
