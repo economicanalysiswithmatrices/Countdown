57be33da8e3f15017fd68f77de073e799202b3f5
Chris Finch <33253740+finchchris@users.noreply.github.com>
2018-01-10 11:21:07 +0000

    Update DisplayHighestScore.m

--- DisplayHighestScore.m
+++ DisplayHighestScore.m
@@ -2,7 +2,7 @@
         
     HighestScoreDisplay = findobj('Style','text','-and','Tag','HighestScore'); % finds the highest score text box on the GUI
     
-    HighestScoreFile = fopen('HighestScorer.txt', 'rt'); % opens the HighestScorer.txt file which contaians the previous highest score
+    HighestScoreFile = fopen('HighestScorer.txt', 'rt'); % opens the HighestScorer.txt file which contains the previous highest score
 
     HighestScorer = textscan(HighestScoreFile, '%s', 'delimiter', '\n'); % searches through the file string array
 
