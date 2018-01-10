9d9711804baf1ad55508955899a39663d783e62b
Chris Finch <33253740+finchchris@users.noreply.github.com>
2018-01-10 17:59:57 +0000

    Linux compatibility
    Added Linux compatibility

--- RunMe.m
+++ RunMe.m
@@ -1,7 +1,7 @@
-if ismac
-    MacGUI
-elseif ispc
-    WindowsGUI
+if ismac || isunix  %If linux or MacOs
+    MacGUI %Run mac version as it works on both
+elseif ispc %If windows PC
+    WindowsGUI %Run windows GUI
 else
     disp('Platform not supported')
 end 
