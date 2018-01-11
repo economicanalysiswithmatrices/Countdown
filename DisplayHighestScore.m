fba395e307a7e2384d994e35b6c2b12b7a24e08e
Chris Finch <33253740+finchchris@users.noreply.github.com>
2018-01-11 22:20:14 +0000

    Update DisplayHighestScore.m

--- DisplayHighestScore.m
+++ DisplayHighestScore.m
@@ -1,13 +1,13 @@
-    function DisplayHighestScore 
+    function DisplayHighestScore % displays the highest score 
         
     HighestScoreDisplay = findobj('Style','text','-and','Tag','HighestScore'); % finds the highest score text box on the GUI
     
     HighestScoreFile = fopen('HighestScorer.txt', 'rt'); % opens the HighestScorer.txt file which contains the previous highest score
 
-    HighestScorer = textscan(HighestScoreFile, '%s', 'delimiter', '\n'); % searches through the file string array
+    HighestScorer = textscan(HighestScoreFile, '%s', 'delimiter', '\n'); % searches through the file 
 
-    if(~isempty(HighestScorer{1,1})) % checks if Cell is empty if not
-        HighestScorer = HighestScorer{1,1}{1,1}; % gets Highest scorer from Cell
+    if(~isempty(HighestScorer{1,1})) % checks if the cell is empty, if not...
+        HighestScorer = HighestScorer{1,1}{1,1}; % gets highest scorer from Cell
         HighestScorer =  strrep(HighestScorer, '_', ' ');  % remove Spaces and replaces _
         set(HighestScoreDisplay, 'String',HighestScorer) % changes 'HighestScoreDisplay' to show highest score
     end
